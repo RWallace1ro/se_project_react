efb8862098e55cd284b167f5edc3831f6375d822
RWallace1ro <wal1ro@aol.com>
2024-02-08 22:28:17 -0500

    Updated Weather.js

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -1,15 +1,21 @@
 import { weatherConditions } from "../../utils/constants";
+import React, { useContext } from "react";
+import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureUnitContext";
 
 const WeatherCard = ({ day, type, weatherTemp = 0 }) => {
   const imageSrc = weatherConditions.filter((i) => {
     return i.day === day && i.type === type;
   });
 
+  const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
+
   const imageSrcUrl = imageSrc[0].url || "";
 
   return (
     <section className="weather" id="weather">
-      <div className="weather_info">{weatherTemp}&deg; F</div>
+      <div className="weather_info">
+        {weatherTemp}&deg; {currentTemperatureUnit}{" "}
+      </div>
       <img src={imageSrcUrl} className="weather_image" alt="weather" />
     </section>
   );
